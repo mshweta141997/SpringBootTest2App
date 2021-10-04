233bec1c30fc534e4cb822727c720e12418395cf
mshweta141997 <mshweta141997@gmail.com>
2021-10-04 17:50:41 +0530

    first commit

---
+++ src/main/java/in/nareshit/ragh/SpringBootTest2AppApplication.java
@@ -0,0 +1,13 @@
+package in.nareshit.ragh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest2AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nareshit/ragh/SpringBootTest2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nareshit.ragh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTest2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
